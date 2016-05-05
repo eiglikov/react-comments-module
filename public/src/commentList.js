c6998634715649652f74a93967539e5a32ac7975
Erik Iglikov <erik.iglikov@jam3.com>
2016-05-05 18:01:18 -0400

    Reactive state

--- public/src/commentList.js
+++ public/src/commentList.js
@@ -1,12 +1,32 @@
 // CommentBox
 let CommentBox = React.createClass({
+  getInitialState: function(){
+    return {data: []};
+  },
+  loadCommentsFromServer: function() {
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      cache: false,
+      success: function(data) {
+        this.setState({data: data});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
+  componentDidMount: function() {
+    this.loadCommentsFromServer();
+    setInterval(this.loadCommentsFromServer, this.props.pollInterval);
+  },
   render: function() {
     return (
-
       <div className="commentBox">
         <div className="jumbotron">
           <div class="container">
             <h2>Comments</h2>
+            <CommentList data={this.state.data} />
           </div>
         </div>
 
@@ -15,11 +35,14 @@ let CommentBox = React.createClass({
       </div>
     );
   }
+
+
 });
 
 // CommentList
 let CommentList = React.createClass({
   render: function() {
+    console.log(this.props.data);
     var commentNodes = this.props.data.map(function(comment) {
       return (
         <Comment author={comment.author} key={comment.id}>
@@ -70,7 +93,7 @@ let Comment = React.createClass({
 
 
 ReactDOM.render(
-  <CommentBox url="/api/comments" />,
+  <CommentBox url="/api/comments" pollInterval={2000}/>,
   document.getElementById('content')
 );
 
